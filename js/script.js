3c4668eeec3c89fa71578a75cd1f7abf7db61c07
Jorge <jorge.saleme77@gmail.com>
2025-08-10 14:18:15 -0600

    feat: add js to mobile hamburguer menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-console.log("Hello World!"); // Fixing flexbox gap property missing in some Safari versions
-
+// SET CURRENT YEAR
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+// MAKE MOBILE NAV WORK
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 function checkFlexGap() {
   var flex = document.createElement("div");
