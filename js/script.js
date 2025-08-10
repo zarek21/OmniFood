045f5c0bed97062128981803f62ff7268ee9b4ff
Jorge <jorge.saleme77@gmail.com>
2025-08-10 15:57:09 -0600

    feat: finish sticky bar

--- js/script.js
+++ js/script.js
@@ -41,6 +41,31 @@ allLinks.forEach(function (link) {
   });
 });
 
+// STICKY NAV
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
